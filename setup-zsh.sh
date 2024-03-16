74aac40d21f4bca67ea5fbc5826d61460492a354
Lutz Warnecke <lutz.warnecke@posteo.de>
2024-03-16 21:01:52 +0100

    fix setup script

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/bash
 
+set -e
+
 ### Copies zsh files in place. Does a backup of existing files.
 
 HOME="/home/$USER"
 
-for file in ".zsh" ".zsh_aliases" ".p10k.zsh"
+for file in ".zshrc" ".zsh_aliases" ".p10k.zsh"
 do
-    if [[ -f "$HOME/$file" ]]; then
-        cp -ab "$file" "$HOME/$file"
-    fi
-done 
+    cp -ab "$file" "$HOME/$file"
+done
 
-if [[ -d "$HOME/.zsh" ]]; then
-    cp -ab ".zsh" "$HOME/.zsh"
-fi
+cp -a ".zsh" "$HOME/.zsh"
